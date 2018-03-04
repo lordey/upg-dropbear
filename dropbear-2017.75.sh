bd0a952dd45a63672bddb597e68294eebceaf9ec
lordey <lord.dey68@gmail.com>
2018-03-05 01:12:26 +0700

    first commit

--- dropbear-2017.75.sh
+++ dropbear-2017.75.sh
@@ -14,45 +14,9 @@ rm -rf /etc/dropbear
 cd .. 
 ls
 dpkg -i dropbear_2017.75-0.1_i386.deb
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
 
-# konfigurasi dropbear
-cat > /etc/default/dropbear <<-END
-# disabled because OpenSSH is installed
-# change to NO_START=0 to enable Dropbear
-NO_START=0
+# rebuild konfigurasi dropbear
+wget https://raw.githubusercontent.com/lordey/konfigurasi-dropbear/master/dropbear-conf.sh && chmod +x dropbear-conf.sh && ./dropbear-conf.sh
 
-# the TCP port that Dropbear listens on
-DROPBEAR_PORT=109
-
-# any additional arguments for Dropbear
-DROPBEAR_EXTRA_ARGS="-p 110 -p 22507 -p 53"
-
-# specify an optional banner file containing a message to be
-# sent to clients before they connect, such as "/etc/issue.net"
-DROPBEAR_BANNER="/etc/issue.net"
-
-# RSA hostkey file (default: /etc/dropbear/dropbear_rsa_host_key)
-#DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
-
-# DSS hostkey file (default: /etc/dropbear/dropbear_dss_host_key)
-#DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
-                              
-# Receive window size - this is a tradeoff between memory and
-# network performance
-DROPBEAR_RECEIVE_WINDOW=65536
-
-
-
-
-
-
-
-END
-
-# restart dropbear
-service ssh restart
-service dropbear restart
-
+# end
 
